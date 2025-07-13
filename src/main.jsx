d7e445827c4b3e56f279c33ef395a4b1420b0b5a
Huynh Sinh <huynhvansinh1810@gmail.com>
2025-07-13 18:39:43 +0700

    Đổi trang Land

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,7 @@ import UserManagementLayout from './pages/UserManagement';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div>
-      {/* <LandingPage/> */}
-      <UserManagementLayout/>
+      <LandingPage/>
     </div>
   </StrictMode>,
 )
